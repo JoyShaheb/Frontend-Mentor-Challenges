8f7697bf5538cd581299a8a888ef30c969f8925f
JoyShaheb <61899866+JoyShaheb@users.noreply.github.com>
2020-11-13 11:11:24 +0600

    c-1

--- Testimonial-Slider/src/App.js
+++ Testimonial-Slider/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import "./styles.css";
+import "./styles.scss";
+import pattern_bg from "./images/pattern-bg.svg";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="container">
+        {/* <img src={pattern_bg} alt="" /> */}
+        <div className="left">
+          <p>
+            “ If you want to lay the best foundation possible I’d recommend
+            taking this course. The depth the instructors go into is incredible.
+            I now feel so confident about starting up as a professional
+            developer. ”
+          </p>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
   );
 }
